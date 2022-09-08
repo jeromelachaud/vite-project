64f2c4177836d32f3e34a4d5f9e3acf166e035b7
Jerome Lachaud <jerome.lachaud@gmail.com>
2022-09-08 09:19:59 +0200

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/button-has-type': 'off',
+    // 'no-shadow': 'off',
+    // '@typescript-eslint/no-shadow': 'warn',
+  },
+};
